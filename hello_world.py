7ddded38cf553a478509b2f760c060978a2f20e3
Sindhura Mente <sindhuramente@gmail.com>
2024-12-13 18:31:58 -0500

    practicing python

---
+++ hello_world.py
@@ -0,0 +1,29 @@
+
+# this is a print statement
+print("hi") 
+
+'''this
+is a multiline
+comment'''
+
+# Variables
+
+var = 1 # this is an integer
+print(var) # the value of var is 1
+print("the value of var is " + str(var)) # the + sign in python is used only for concatenating strings
+print("the value of var is", var)
+var1 = 3
+print("var =", var, "var1 =", var1) # example of chaining variables in a print statement
+
+my_string = _ # this is a string
+my_string1 = _
+
+# print the value of both strings on one line
+
+# print the value of both string on separate lines
+
+# print the value of both strings on separate lines using one print statement
+
+# print out the first character of one of the strings
+
+# print out a substring of length 3 from one of the strings
